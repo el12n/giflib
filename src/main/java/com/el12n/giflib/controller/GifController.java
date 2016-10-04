ae598d3664f1cad5125a3891d7077b1e79997021
Alvaro Lockhart <alvarolock@gmail.com>
2016-10-04 08:42:51 -0400

    Remove unused import

--- src/main/java/com/el12n/giflib/controller/GifController.java
+++ src/main/java/com/el12n/giflib/controller/GifController.java
@@ -4,7 +4,6 @@ import com.el12n.giflib.model.Gif;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.Date;
 
